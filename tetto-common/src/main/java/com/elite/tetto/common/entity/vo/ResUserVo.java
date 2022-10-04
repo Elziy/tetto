acc6cbaf90ebad8cb47d4ca2aac3573967aa73a7
xyl <1761049206@qq.com>
2022-10-04 19:18:46 +0800

    添加1.0

--- tetto-common/src/main/java/com/elite/tetto/common/entity/vo/ResUserVo.java
+++ tetto-common/src/main/java/com/elite/tetto/common/entity/vo/ResUserVo.java
@@ -2,6 +2,12 @@ package com.elite.tetto.common.entity.vo;
 
 import lombok.Data;
 
+/**
+ * 注册用户信息
+ *
+ * @author 17610
+ * @date 2022/10/04
+ */
 @Data
 public class ResUserVo {
     private String email;
